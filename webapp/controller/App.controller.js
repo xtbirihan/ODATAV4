757cfa435b6d213c53b806e5df0cd575c163f4e8
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-22 10:35:06 +0000

    Sample: OData V4 - Step 2 - Data Access and Client-Server Communication

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+	"sap/m/MessageBox",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+], function (Controller, MessageToast, MessageBox, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -15,6 +17,18 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
-		}
+		},
+        onRefresh : function () {
+            var oBinding = this.byId("peopleList").getBinding("items");
+            if (oBinding.hasPendingChanges()) {
+				MessageBox.error(this._getText("refreshNotPossibleMessage"));
+				return;
+            }
+            oBinding.refresh();
+            MessageToast.show(this._getText("refreshSuccessMessage")); 
+        },
+        _getText : function (sTextId, aArgs) { 
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs); 
+        }
 	});
 });
