5e68e05012963b9bc1f407eedee072f96cb28695
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-22 09:21:38 +0000

    Sample: OData V4 - Step 1 - Set Up the Initial App

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sap/ui/core/tutorial/odatav4/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapuicoretutorial/odatav4/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
