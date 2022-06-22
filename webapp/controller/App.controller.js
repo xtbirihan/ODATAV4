b73b36f783a2479d0a4ad1dac921abd89fa20337
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-22 11:22:14 +0000

    Sample: OData V4 - Step 4 - Filtering, Sorting, and Counting

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,34 +1,61 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
+    "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-	"sap/m/MessageBox",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
-	"use strict";
+    "sap/m/MessageBox",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterType",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel) {
+    "use strict";
 
-	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
+    return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
 
 		/**
 		 *  Hook for initializing the controller
 		 */
-		onInit : function () {
-			var oJSONData = {
-				busy : false
-			};
-			var oModel = new JSONModel(oJSONData);
-			this.getView().setModel(oModel, "appView");
-		},
-        onRefresh : function () {
+        onInit: function () {
+            var oJSONData = {
+                busy: false,
+                order: 0
+            };
+            var oModel = new JSONModel(oJSONData);
+            this.getView().setModel(oModel, "appView");
+        },
+        onRefresh: function () {
             var oBinding = this.byId("peopleList").getBinding("items");
             if (oBinding.hasPendingChanges()) {
-				MessageBox.error(this._getText("refreshNotPossibleMessage"));
-				return;
+                MessageBox.error(this._getText("refreshNotPossibleMessage"));
+                return;
             }
             oBinding.refresh();
-            MessageToast.show(this._getText("refreshSuccessMessage")); 
+            MessageToast.show(this._getText("refreshSuccessMessage"));
         },
-        _getText : function (sTextId, aArgs) { 
-            return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs); 
+        onSearch: function () {
+            var oView = this.getView(),
+                sValue = oView.byId("searchField").getValue(),
+                oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+            oView.byId("peopleList").getBinding("items").filter(oFilter, FilterType.Application);
+        },
+        onSort : function () {
+            var oView = this.getView(),
+                aStates = [undefined, "asc", "desc"],
+                aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+                sMessage, 
+                iOrder = oView.getModel("appView").getProperty("/order"); 
+
+                iOrder = (iOrder + 1 ) % aStates.length;
+
+            var sOrder = aStates[iOrder]; 
+            oView.getModel("appView").setProperty("/order", iOrder); 
+            oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+            sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+            MessageToast.show(sMessage);
+        },
+
+        _getText: function (sTextId, aArgs) {
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
         }
-	});
+    });
 });
